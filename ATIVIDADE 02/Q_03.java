e43960935bd0f281233a1849c8d6e56b07d3c76f
lucascoelho33 <61910750+lucascoelho33@users.noreply.github.com>
2023-09-14 11:11:01 -0300

    Add files via upload

---
+++ ATIVIDADE 02/Q_03.java
@@ -0,0 +1,33 @@
+public class Cliente {
+    private int id;
+    private String nome;
+    private String endereco;
+    private double saldo;
+
+    public void exibirInformacoes() {
+        // exibir informações do cliente
+    }
+
+    public void atualizarSaldo(){
+        // atualizar saldo do cliente
+    } 
+}
+
+//-------------------------------------------------------------//
+public class Compra {
+    private double valorCompra;
+
+    public void realizarCompra(){
+        // lógica de compra
+    }
+}
+
+//--------------------------------------------------------------//
+public class Conta {
+    private String numeroConta;
+    private double saldo;
+
+    public void atualizarSaldo(){
+        
+    }
+}
